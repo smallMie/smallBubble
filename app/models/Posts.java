28a745eb5212547ef83004c4414f2836284591f4
name <mall.com>
2016-08-13 23:10:22 +0800

    POST

--- app/models/Posts.java
+++ app/models/Posts.java
@@ -1,5 +1,36 @@
 package models;
 
-public class Posts {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+import play.data.validation.Required;
+import play.db.jpa.Model;
+
+@Entity
+@Table(name="posts")
+public class Posts extends Model {
+	//标题
+	@Required
+	@Column(name="post_title",nullable=false)
+    public String postTitle;
+	//内容
+	@Required
+	@Column(name="post_content",nullable=false)
+    public String postContent;
+	//作者
+	@Required
+	@Column(name="post_owner_id",nullable=false)
+    public String postOwnerId;
+	//日期
+	@Required
+	@Column(name="post_creat_time",nullable=true)
+    public String postCreatTime;
+	//状态
+	@Required
+	@Column(name="post_status",nullable=true)
+    public String postStatus;
+	//标识
+	@Required
+	public long flag;
 }
